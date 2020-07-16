3d9dffb3d329d1248ea0c0956445039020501d2b
Naveenkumar <paramnav26@gmail.com>
2020-07-16 14:01:25 +0200

    Deleted parties folder

--- PARTIES/src/Turb_model/Include/Subgrid.h
+++
@@ -1,8 +0,0 @@
-#ifndef SUBGRID_H
- #define SUBGRID_H
-Subgrid *Subgrid_create(MAC_grid *grid, Parameters *params) ;
-void Subgrid_concentration_derivative(Concentration *conc, MAC_grid *grid,
-		Parameters *params, Subgrid *smag, int idir);
-void Subgrid_smagorinsky_eddy_viscosity(Cart3d_bag *data_bag);
-void Subgrid_boundary(double ***var, MAC_grid *grid, Parameters *params);
-#endif
