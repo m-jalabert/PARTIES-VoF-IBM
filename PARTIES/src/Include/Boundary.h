970df123db2e35ca1d358e0188209292550fed2a
fabiankleischmann <f.kleischmann@tu-bs.de>
2023-01-19 15:17:09 +0100

    Added comments

--- PARTIES/src/Include/Boundary.h
+++ PARTIES/src/Include/Boundary.h
@@ -169,9 +169,9 @@
 #undef  BICG_SOLVE // nouniform mesh
 #undef  TEST
 
-#define OSCILLATION        		// Oscillation force 
+#define OSCILLATION        		// Oscillation force for oscillating the domain/fluid container
 
-#undef OSCILLATING_PARTICLE		// Particle oscillates with prescribed u-velocity
+#undef OSCILLATING_PARTICLE		// Oscillation of the particle with prescribed u-velocity
 
 #undef STOKES_2ND_PROBLEM		// Oscillating boundary layer due to oscillating Top_Wall
 								// Required: 	#define  TOP_WALL_VELOCITY
