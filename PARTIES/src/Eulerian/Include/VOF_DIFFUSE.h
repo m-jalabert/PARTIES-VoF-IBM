5280923b96f2e9f7fc0a911323e677456d815652
Maxime Jalabert <maxime.jalabert@outlook.com>
2026-04-13 14:51:39 -0400

    Cahn Hillard VoF header added

---
+++ PARTIES/src/Eulerian/Include/VOF_DIFFUSE.h
@@ -0,0 +1,33 @@
+#ifndef VOF_DIFFUSE_H
+#define VOF_DIFFUSE_H
+
+#include "DataTypes.h"
+
+#ifdef VOF_DIFFUSE
+void VOF_DIFFUSE_init(Cart3d_bag *db);
+void VOF_DIFFUSE_set_boundary_values(double ***f, Cart3d_bag *db);
+
+void VOF_DIFFUSE_compute_C_S(Cart3d_bag *db);
+void VOF_DIFFUSE_compute_laplacian(double ***in, double ***lap, Cart3d_bag *db);
+void VOF_DIFFUSE_compute_bulk_S(Cart3d_bag *db);
+void VOF_DIFFUSE_compute_psi(Cart3d_bag *db);
+void VOF_DIFFUSE_compute_psi_LG(Cart3d_bag *db);
+
+void VOF_DIFFUSE_advect_WENO5(Cart3d_bag *db, double ***rhs_out);
+void VOF_DIFFUSE_explicit_diffusion(Cart3d_bag *db, double ***rhs_out);
+void VOF_DIFFUSE_solve_implicit_biharmonic(Cart3d_bag *db, double ***rhs_explicit);
+void VOF_DIFFUSE_step(Cart3d_bag *db);
+
+void VOF_DIFFUSE_build_mcl_mask(Cart3d_bag *db);
+void VOF_DIFFUSE_apply_contact_angle(Cart3d_bag *db);
+
+void VOF_DIFFUSE_compute_f_sigma(Cart3d_bag *db);
+void VOF_DIFFUSE_apply_f_sigma_old(Cart3d_bag *db);
+
+void VOF_DIFFUSE_update_density_viscosity(Cart3d_bag *db);
+void VOF_DIFFUSE_capillary_force_hybrid(Cart3d_bag *db);
+
+
+#endif
+
+#endif
